bbb03cab58c3264c229db8e41be5ea02a34dbfef
zl810881283 <810881283@qq.com>
2016-04-23 00:49:32 +0800

    fix re

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,54 +2,36 @@
  * Created by chuandong on 15/12/17.
  */
 
-var counter = 0;
+$(function () {
+  $(window).scroll(function () {
+    var scrollPos = $(this).scrollTop();
 
-setInterval(function(){
-    counter += 1;
+    if ($(window).scrollTop() > 70) {
+      $('#site-header-id').addClass('site-header-nav-scrolled');
+    } else {
+      $('#site-header-id').removeClass('site-header-nav-scrolled');
+    }
+  });
 
-    $("#chuan-title").fadeOut(1000, function() {
-        switch (counter % 6) {
-            case 0:
-                $("#chuan-title").html("Chuan is so cool, right?");
-                break;
-            case 1:
-                $("#chuan-title").html("Chuan plans to be a Full Stack developer");
-                break;
-            case 2:
-                $("#chuan-title").html("Chuan dreams to be a CTO ");
-                break;
-            case 3:
-                $("#chuan-title").html("Chuan is now fighting with AngularJS");
-                break;
-            case 4:
-                $("#chuan-title").html("Chuan loves animals and traveling");
-                break;
-            case 5:
-                $("#chuan-title").html("Chuan has girlfriend now, so not available :D");
-                break;
-            default:
-                $("#chuan-title").html("Cool Boy");
-                break;
-        }
-    });
-    $("#chuan-title").fadeIn(1000);
-
-}, 2000);
-
-// Window Scroll
-var windowScroll = function () {
-    $(window).scroll(function () {
-
-        var scrollPos = $(this).scrollTop();
-
-        if ($(window).scrollTop() > 70) {
-            $('#site-header-id').addClass('site-header-nav-scrolled');
-        } else {
-            $('#site-header-id').removeClass('site-header-nav-scrolled');
-        }
-    });
-};
-
-$( document ).ready(function() {
-    windowScroll();
+  var titles = [
+    'Za is so cool, right?',
+    'Chuan plans to be a Full Stack developer',
+    'Chuan dreams to be a CTO',
+    'Chuan is now fighting with AngularJS',
+    'Chuan loves animals and traveling',
+    'Chuan has girlfriend now, so not available :D',
+    'Cool Boy',
+  ];
+  var counter = 0;
+  var $title = $("#persol-title");
+  if ($title.length > 0 && titles.length > 0) {
+    $title.html(titles[counter]);
+    setInterval(function () {
+      counter = (counter + 1) % titles.length;
+      $title.fadeOut(1000, function () {
+        $title.html(titles[counter]);
+      });
+      $title.fadeIn(1000);
+    }, 2000);
+  }
 });
