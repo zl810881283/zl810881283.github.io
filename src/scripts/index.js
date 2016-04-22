1d434aeca98034c414bd74487aa66c211bf170c2
zl810881283 <810881283@qq.com>
2016-04-23 01:46:28 +0800

    fix re

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,23 +16,25 @@ $(function () {
 
   var counter = 0;
   var $title = $("#person-title");
-  
-  var titles=[], temp= $title.data('person-titles').split('|');
 
-  for(var i in temp){
-    if(temp[i]){
-      titles.push(temp[i])
+  if ($title.length > 0) {
+    var titles = [], temp = $title.data('person-titles').split('|');
+
+    for (var i in temp) {
+      if (temp[i]) {
+        titles.push(temp[i])
+      }
     }
-  }
-  
-  if ($title.length > 0 && titles.length > 0) {
-    $title.html(titles[counter]);
-    setInterval(function () {
-      counter = (counter + 1) % titles.length;
-      $title.fadeOut(1000, function () {
-        $title.html(titles[counter]);
-      });
-      $title.fadeIn(1000);
-    }, 2000);
+
+    if (titles.length > 0) {
+      $title.html(titles[counter]);
+      setInterval(function () {
+        counter = (counter + 1) % titles.length;
+        $title.fadeOut(1000, function () {
+          $title.html(titles[counter]);
+        });
+        $title.fadeIn(1000);
+      }, 2000);
+    }
   }
 });
